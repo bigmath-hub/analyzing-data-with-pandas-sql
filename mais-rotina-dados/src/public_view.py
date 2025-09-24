1d511d35b4296d9c084646875d0ec55d62b78978
bigmath <108194820+bigmath-hub@users.noreply.github.com>
2025-09-24 10:56:04 +0000

    public_view

---
+++ mais-rotina-dados/src/public_view.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def public_vagas(path_o="data/samples/oportunidades.csv", path_c="data/outputs/candidaturas.csv") -> pd.DataFrame:
+    
+    
+    
+    # cands_sugestao: num. de linhas da candidatura com origem == "sugestao"
+
+    # cands_sorteio: num. de linhas da candidatura com origem == "sorteio"
+
+    # cands_total: soma
+
+    # top1_cand_id: cand_id do maior score daquela vaga
+
+    # top1_score: score do candidato
+
+    
+    
+
+
+def main():
+    print(public_vagas())
+
+
+if __name__ == "__main__":
+    main()
