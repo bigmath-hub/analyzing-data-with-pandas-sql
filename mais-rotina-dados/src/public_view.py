81def14370d9bebf61e4bc5de20f13576d3944ce
bigmath <108194820+bigmath-hub@users.noreply.github.com>
2025-10-01 09:04:03 +0000

    cadidatos

--- mais-rotina-dados/src/public_view.py
+++ mais-rotina-dados/src/public_view.py
@@ -5,10 +5,17 @@ def public_vagas(path_o="data/samples/oportunidades.csv", path_c="data/outputs/c
 
     df_o = pd.read_csv(path_o, dtype={"id": "string", "status": "string", "agencia_id": "string"})
     df_c = pd.read_csv(path_c, dtype={"vaga_id": "string", "cand_id": "string", "score": float})        
-    cont_cand = df_c.groupby('vaga_id')['origem'].value_counts().unstack().reindex(columns=["sugestao","sorteio"], fill_value=0)
-    print(cont_cand)  
+    cont_cand = df_c.groupby('vaga_id')['origem'].value_counts().unstack().reindex(columns=["sugestao","sorteio"], fill_value=0)    
     melhores_linhas = df_c.loc[df_c.groupby('vaga_id')['score'].idxmax()]
     top1_cand = melhores_linhas.set_index('vaga_id')[['cand_id', 'score']]        
+    
+    print(df_o) # id
+    print(df_o.dtypes) # id
+    print()
+    print(cont_cand) # vaga_id
+    print(cont_cand.dtypes) # vaga_id
+
+    
 
     df_merge1 = pd.merge(df_o, cont_cand, how='left', left_on='id', right_index=True)
     df_merge1[['sugestao', 'sorteio']] = df_merge1[['sugestao', 'sorteio']].fillna(0).astype(int)
@@ -38,20 +45,41 @@ def public_vagas(path_o="data/samples/oportunidades.csv", path_c="data/outputs/c
 def public_candidatos(
         p_candidatos="data/samples/candidatos.csv", 
         p_candidaturas="data/outputs/candidaturas.csv", 
-        p_opotunidades="data/samples/oportunidades.csv"
+        p_oportunidades="data/samples/oportunidades.csv"
 ) -> pd.DataFrame:
     
-    df_candidatos = pd.read_csv(p_candidaturas, dtype={
+    df_candidaturas = pd.read_csv(p_candidaturas, dtype={
         "vaga_id": "string", "cand_id": "string", "score": float, "origem": "string", "status": "string"
     })
-       
+
+    df_candidatos = pd.read_csv(p_candidatos, dtype="string")
+    df_oportunidades = pd.read_csv(p_oportunidades, dtype="string")
+    
+    
     contagem_origem = (
-        df_candidatos.groupby("cand_id")['origem']
+        df_candidaturas.groupby("cand_id")['origem']
         .value_counts()
         .unstack()
         .reindex(columns=['sugestao', 'sorteio'],fill_value=0)
         .fillna(0).astype(int) # limpar na origem dessa vez
-    )
+    )    
+        
+    contagem_origem['total_vagas'] = contagem_origem.sum(axis=1) 
+    print(contagem_origem)
+    
+    melhores_linhas = df_candidaturas.iloc[df_candidaturas.groupby('vaga_id')['score'].idxmax()]
+    print(melhores_linhas)
+    
+    df_intermediario = melhores_linhas.drop(['cand_id', 'status', 'origem'], axis=1) 
+    
+    #df_merge = pd.merge(df_oportunidades, df_intermediario, how='left', left_on='id', right_index=True)
+    
+    
+    
+    
+
+    
+
     
 
     
@@ -63,8 +91,8 @@ def public_candidatos(
 
 def main():
 
-    public_candidatos()   
     
+    public_candidatos()   
 
 
 
