23582e3f0157c88925016d570b15a822eb89cc10
bigmath <108194820+bigmath-hub@users.noreply.github.com>
2025-10-08 09:59:36 +0000

    public_vagas

--- mais-rotina-dados/src/public_view.py
+++ mais-rotina-dados/src/public_view.py
@@ -14,7 +14,6 @@ def public_vagas(path_o="data/samples/oportunidades.csv", path_c="data/outputs/c
     print()
     print(cont_cand) # vaga_id
     print(cont_cand.dtypes) # vaga_id
-
     
 
     df_merge1 = pd.merge(df_o, cont_cand, how='left', left_on='id', right_index=True)
@@ -48,88 +47,76 @@ def public_candidatos(
         p_oportunidades="data/samples/oportunidades.csv"
 ) -> pd.DataFrame:
     
-    df_candidaturas = pd.read_csv(p_candidaturas, dtype={
-        "vaga_id": "string", "cand_id": "string", "score": float, "origem": "string", "status": "string"
-    })
-
-    df_candidatos = pd.read_csv(p_candidatos, dtype="string") # linha inutil?
-    df_oportunidades = pd.read_csv(p_oportunidades, dtype="string")
-    
-    
-    # contagem por origem e totais
-    contagem_origem = (
-        df_candidaturas.groupby("cand_id")['origem']
-        .value_counts()
-        .unstack()
-        .reindex(columns=['sugestao', 'sorteio'],fill_value=0)
-        .fillna(0).astype(int) # limpar na origem dessa vez
-    )        
-    contagem_origem['total_vagas'] = contagem_origem.sum(axis=1)    
-    
-    
-    # informacoes da melhor vaga
-    melhores_linhas = df_candidaturas.iloc[df_candidaturas.groupby('cand_id')['score'].idxmax()]        
-    cols_interesse = ['cand_id', 'vaga_id', 'score', ] # limpar o df para manter as colunas que me interessam
-    df_intermediario = melhores_linhas[cols_interesse]    
-    df_titulo = pd.merge(df_intermediario, df_oportunidades, left_on='vaga_id', right_on='id') # merge       
-    cols_finais = ['cand_id', 'score', 'vaga_id', 'titulo'] 
-    top_vaga_info = df_titulo[cols_finais] # construir o df top_vagas_info
-    
-
-    df_final = df_candidatos    
-    # left merge - trazer as contagens
-    df_final = pd.merge(
-        df_final,
-        contagem_origem,
-        how='left',
-        left_on='id',
-        right_index=True
+    df_candidaturas = pd.read_csv( 
+        p_candidaturas,
+        dtype={"vaga_id": "string", "cand_id": "string", "score": float, "origem": "string", "status": "string"}        
     )
-
-    # left merge - trazer informacoes top vaga
-    df_final = pd.merge(
-        df_final,
-        top_vaga_info,
-        how='left',
-        left_on='id',
-        right_on='cand_id'
-    )    
+    df_candidatos = pd.read_csv(p_candidatos, dtype="string") 
+    df_oportunidades = pd.read_csv(p_oportunidades, dtype="string")  
     
     
-    df_final = df_final.drop('cand_id', axis=1)
+    if df_candidaturas.empty:
+        contagem_origem = pd.DataFrame(0, index=df_candidatos['id'], colunms=['sugestao', 'sorteio'])
+        top_vaga_info = pd.DataFrame(colunms=['cand_id', 'vaga_id', 'titlulo', 'score'])    
+    else:       
+        # contagem por origem e totais
+        contagem_origem = (df_candidaturas.groupby("cand_id")['origem']            
+            .value_counts()
+            .unstack()            
+            .reindex(columns=['sugestao', 'sorteio'],fill_value=0)
+            .fillna(0)
+            .astype(int) # limpar na origem dessa vez
+        )        
+        idx = df_candidaturas.groupby('cand_id')['score'].idxmax()
+        melhores_linhas = df_candidaturas.loc[idx, ["cand_id", "vaga_id", "score"]]
+        
+        top_vaga_info = melhores_linhas.merge(
+            df_oportunidades[["id", "titulo"]],
+            left_on="vaga_id",
+            right_on="id",
+            how="left",
+        ).drop(columns=["id"])
 
-    # limpeza final    
-    colunas_int = ['sugestao', 'sorteio', 'total_vagas']
-    df_final[colunas_int] = df_final[colunas_int].fillna(0).astype(int)
-    df_final['score'] = df_final['score'].fillna(0.0).astype(float)
-    colunas_texto = ['vaga_id', 'titulo']
-    df_final[colunas_texto] = df_final[colunas_texto].fillna('').astype('string')    
+        contagem_origem = contagem_origem.reindex(df_candidatos["id"], fill_value=0)
+        contagem_origem["inscricoes_total"] = contagem_origem.sum(axis=1)
 
-    colunas_renomear = {
-    'id': 'cand_id',
-    'sugestao': 'total_sugestao',
-    'sorteio': 'total_sorteio',
-    'vaga_id': 'top_vaga_id',
-    'titulo': 'top_vaga_titulo',
-    'score': 'top_score'
-    }
+        df_final = (
+            df_candidatos
+            .merge(contagem_origem, how="left", left_on="id", right_index=True)
+            .merge(top_vaga_info, how="left", left_on="id", right_on="cand_id")
+            .drop(columns=["cand_id"])  # veio do merge do top_vaga_info
+        )
 
-    df_final = df_final.rename(columns=colunas_renomear)
-    
-    colunas_finais = [
-    'cand_id', 'nome', 'idiomas', 'regioes_preferidas', 'tipo',
-    'total_vagas',
-    'total_sugestao', 'total_sorteio',
-    'top_vaga_id', 'top_vaga_titulo', 'top_score'
-    ]   
+        for col in ["sugestao", "sorteio", "inscricoes_total"]:
+            df_final[col] = df_final[col].fillna(0).astype(int)
 
-    return df_final[colunas_finais]
+        df_final["score"] = df_final["score"].fillna(0.0).astype(float)
+        for col in ["vaga_id", "titulo"]:
+            df_final[col] = df_final[col].fillna("").astype("string")
 
+        df_final = df_final.rename(columns={
+            "id": "cand_id",
+            "sugestao": "total_sugestao",
+            "sorteio": "total_sorteio",
+            "vaga_id": "top_vaga_id",
+            "titulo": "top_vaga_titulo",
+            "score": "top_score",
+        })
 
+        colunas_finais = [
+            "cand_id", "nome", "idiomas", "regioes_preferidas", "tipo",
+            "total_sugestao", "total_sorteio", "inscricoes_total",
+            "top_vaga_id", "top_vaga_titulo", "top_score",
+        ]
+
+        return df_final[colunas_finais]
 
 def main():    
-    print(public_candidatos())
-
+    # objetivo: encontrar os 5 candidatos com > inscricoes_total
+    candidatos = public_candidatos()
+    maiores_cand = candidatos.nlargest(5, columns='inscricoes_total') # aqui eu tenho todo o df
+    print(maiores_cand[['cand_id', 'top_vaga_id', 'top_score']]) # eu selecionei apenas essas tres colunas contendo algumas informacoes relevantes
+    
 
 if __name__ == "__main__":
     main()
